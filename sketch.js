2cc8b3c9ea977dc347906dbee87cdcd083ba97dc
rishikab27 <73723554+rishikab27@users.noreply.github.com>
2021-03-14 09:50:43 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var hr,mm,sc;
+
+function setup() {
+  createCanvas(400,400);
+  
+  angleMode(DEGREES);
+  
+}
+
+function draw() {
+  background('black'); 
+  translate(200,200)
+  rotate(-90); 
+
+  hr = hour();
+  mm = minute();
+  sc = second();
+
+
+  scAngle = map(sc, 0, 60, 0, 360)
+  mmAngle =  map(mm,0,60,0,360);
+  hrAngle = map(hr%12 , 0,12,0,360);
+
+
+  push();
+  rotate(scAngle);
+  stroke (255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop ()
+
+  push();
+  rotate(mmAngle);
+  stroke (0,255,0);
+  strokeWeight(7);
+  line(0,0,80,0);
+  pop ()
+
+  push();
+  rotate(hrAngle);
+  stroke (0,0,255);
+  strokeWeight(7);
+  line(0,0,60,0);
+  pop ();
+
+
+  strokeWeight(10);
+  noFill();
+  stroke(255,0,0);
+  arc(0,0,300,300,0,scAngle);
+  
+
+  stroke(0,255,0)
+  arc(0,0,280,280,0,mmAngle)
+
+  stroke(0,0,255);
+  arc(0,0,260,260,0,hrAngle)
+  drawSprites();
+}
